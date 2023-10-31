8106563696853ad3f0c1dea7715e0f5249fbe98c
jimmy <theman21@zpay.or.kr>
2023-10-31 10:10:04 +0900

    앱 아키텍처 적용
    - 네트워크 모듈 클래스, Timber 적용

---
+++ app/src/main/java/com/deviz/couponhunter/di/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.deviz.couponhunter.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class NetworkModule {
+
+
+
+
+}
